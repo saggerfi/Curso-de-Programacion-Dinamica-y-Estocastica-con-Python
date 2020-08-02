25f25c45b10fe5b269b9ee5e0911dfc780c7e3f5
sagger <yousagger@gmail.com>
2020-08-02 15:18:43 -0500

    Desafio completo

--- desafio_color.py
+++ desafio_color.py
@@ -3,6 +3,29 @@ import collections
 from baraja import crear_baraja, dar_mano 
 
 
+def color(manos):
+    color = 0
+    for mano in manos:
+        mismo_palo = True
+        palo = []
+        for carta in mano:
+            print(f'carta = {carta}')
+            palo.append(carta[0])
+            if carta[0] == mano[0][0]:
+                pass
+            else:
+                mismo_palo = False
+        
+        if mismo_palo == True:
+            color += 1
+        else:
+            pass
+
+    
+    print(f'Intentos = {intentos}')
+    print(f'Probabilidad de sacar una corrida = {color / intentos}')
+
+
 def simulacion(tamano, intentos):
     barajas = crear_baraja()
     manos = []
@@ -11,24 +34,15 @@ def simulacion(tamano, intentos):
         mano = dar_mano(barajas, tamano)
         manos.append(mano)
     
-    color = 0
-    for mano in manos:
-        valores = []
-        for carta in mano:
-            valores.append(carta[0])
-        
-        contador =  dict(collections.Counter(valores))
-        for val in contador.values():
-            if val == val:
-                color += 1
-                
+    corrida = color(manos)               
 
 if __name__ == '__main__':
     tamano = 0
 
     while tamano == 0 or tamano > 52:
         tamano = int(input('¿De qué tamaño es la mano?: '))
-        print('Número no válido ingrese un número en el intervalo [1 - 52]')
+        if tamano == 0 or tamano > 52:
+            print('Número no válido ingrese un número en el intervalo [1 - 52]')
 
     intentos = int(input('¿Cuántas simulaciones quiere correr para obtener la probabilidad?: '))
     simulacion(tamano, intentos)
